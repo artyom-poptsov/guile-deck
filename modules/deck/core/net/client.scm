47b402acb0e00e3804147caa11f7e500aa40f34e
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-01-30 21:55:23 +0300

    http-client.scm: Rename to 'client.scm'
    * modules/deck/core/net/http-client.scm: Rename to 'client.scm'.
    * modules/deck/core/net/Makefile.am: Update.

---
+++ modules/deck/core/net/client.scm
@@ -0,0 +1,39 @@
+(define-module (deck core net client)
+  #:use-module (srfi srfi-8)
+  #:use-module (ice-9 iconv)
+  #:use-module (oop goops)
+  #:use-module (web client)
+  #:use-module (web uri)
+  #:use-module (web response)
+  #:use-module (json)
+  #:export (<client>
+            client-server-uri
+            client-get
+            client-put
+            client-post
+            uri-parameters->string))
+
+
+(define-class <client> ()
+  ;; <uri>
+  (server-uri
+   #:init-value   #f
+   #:init-keyword #:server
+   #:getter       client-server-uri))
+
+
+(define* (client-get client
+                     resource
+                     #:key
+                     (query #f))
+  (let* ((server (client-server-uri client))
+         (uri    (build-uri (uri-scheme server)
+                            #:host   (uri-host server)
+                            #:port   (uri-port server)
+                            #:path   resource
+                            #:query  query)))
+    (receive (response body)
+        (http-get uri)
+      (and (= (response-code response) 200)
+           (json-string->scm (bytevector->string body "UTF-8"))))))
+
