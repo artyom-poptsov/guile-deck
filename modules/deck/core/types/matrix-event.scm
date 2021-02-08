dd7c20e7fb9c29b2ab39b1ff886cc3764a719e12
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-02-08 20:44:23 +0300

    matrix-event.scm: Improve; add class printers
    * modules/deck/core/types/matrix-event.scm: Improve.  Add class printers.

--- modules/deck/core/types/matrix-event.scm
+++ modules/deck/core/types/matrix-event.scm
@@ -2,9 +2,11 @@
   #:use-module (oop goops)
   #:use-module (srfi srfi-1)
   #:use-module (ice-9 regex)
+  #:use-module (deck core types matrix-id)
   #:export (<matrix-event>
             matrix-event?
             matrix-event-id
+            matrix-event-user-id
             matrix-event-room-id
             matrix-event-sender-id
             matrix-event-type
@@ -20,6 +22,11 @@
    #:init-value   #f
    #:init-keyword #:id
    #:getter       matrix-event-id)
+
+  (user-id
+   #:init-value   #f
+   #:init-keyword #:user-id
+   #:getter       matrix-event-user-id)
 
   ;; <matrix-id>
   (room-id
@@ -47,16 +54,45 @@
 
 
 
+(define-method (display (event <matrix-event>) (port <port>))
+  (format port "#<matrix-event ~a type: ~a ~a>"
+          (if (string? (matrix-event-id event))
+              (matrix-event-id event)
+              (matrix-id->string (matrix-event-id event)))
+          (matrix-event-type event)
+          (number->string (object-address pipe) 16)))
+
+(define-method (write (event <matrix-event>) (port <port>))
+  (display event port))
+
+(define-method (display (event <matrix-event>))
+  (next-method)
+  (display event (current-output-port)))
+
+(define-method (write (event <matrix-event>))
+  (next-method)
+  (display event (current-output-port)))
+
+
+
 (define (matrix-event? object)
   (is-a? object <matrix-event>))
 
 
 
 (define-method (alist->matrix-event (lst <list>))
+  (display lst)
+  (newline)
   (make <matrix-event>
-    #:id        (assoc-ref lst "event_id")
-    #:room-id   (assoc-ref lst "room_id")
-    #:sender-id (assoc-ref lst "sender")
+    #:id        (catch
+                  #t
+                  (lambda ()
+                    (string->matrix-id (assoc-ref lst "event_id")))
+                  (lambda args
+                    (assoc-ref lst "event_id")))
+    #:user-id   (string->matrix-id (assoc-ref lst "user_id"))
+    #:room-id   (string->matrix-id (assoc-ref lst "room_id"))
+    #:sender-id (string->matrix-id (assoc-ref lst "sender"))
     #:type      (assoc-ref lst "type")
     #:content   (assoc-ref lst "content")))
 
