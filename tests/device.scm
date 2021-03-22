8a1062d9c4f55ae6c253339ebd642f48ca926bf2
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-03-22 18:24:01 +0300

    tests/device.scm: Expand the test suite

--- tests/device.scm
+++ tests/device.scm
@@ -1,11 +1,30 @@
 (use-modules (srfi srfi-64)
              (srfi srfi-26)
+             (ice-9 regex)
              (oop goops)
              (deck core types device))
 
 
 
 (test-begin "device")
+
+(test-assert "device?"
+  (and (device? (make <device>
+                  #:id                  "id"
+                  #:display-name        "name"
+                  #:last-seen-ip        "ip"
+                  #:last-seen-timestamp "timestamp"))
+       (not (device? "not a device"))))
+
+(test-assert "display"
+  (let ((output (with-output-to-string
+                  (lambda ()
+                    (display (make <device>
+                               #:id                  "id"
+                               #:display-name        "name"
+                               #:last-seen-ip        "ip"
+                               #:last-seen-timestamp "timestamp"))))))
+    (string-match "#<device id [0-9a-z]+>" output)))
 
 (test-equal "equal?"
   (make <device>
